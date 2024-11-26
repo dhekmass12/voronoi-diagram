e038edfe1db26a46251ccb523c43ee48b61344c8
dhekmass12 <dimas.tri01@ui.ac.id>
2024-11-26 14:12:17 +0700

    [ADD] point input feature from user

--- main.js
+++ main.js
@@ -37,6 +37,8 @@ $(document).ready(function () {
 	$("#voronoi").on("click", function (event) {
 		let x = event.pageX - $(this).offset().left;
 		let y = event.pageY - $(this).offset().top;
+
+        console.log(x, y)
 	
 		/* Add point */
 		let add = true;
@@ -66,6 +68,79 @@ $(document).ready(function () {
 		// event handlers
 		voronoi.addSite(x, y);
 	});
+
+    let userPoints = []
+    document.getElementById('inputfile')
+            .addEventListener('change', function () {
+
+                let fr = new FileReader();
+                fr.onload = function () {
+                    vor.point_list = [];
+                    points = [];
+                    _svg_.textContent = '';
+                    // vor1.set_points([]);
+                    // vor1.reset();
+                    voronoi.clearSites();
+
+                    let text = ""
+                    text = fr.result
+
+                    document.getElementById('output')
+                        .textContent = text
+
+                    userPoints = text.split(")")
+                    userPoints.pop()
+
+                    for (let i = 0; i < userPoints.length; ++i){
+                        let point = userPoints[i]
+                        
+                        if (point.charAt(0) === ","){
+                            userPoints[i] = userPoints[i].slice(2)
+                        }
+                        else{
+                            userPoints[i] = userPoints[i].slice(1)
+                        }
+
+                        point = userPoints[i].split(",")
+                        let x = parseInt(point[0])
+                        let y = parseInt(point[1])
+
+                        userPoints[i] = [x, y]
+                    
+                        /* Add point */
+                        let add = true;
+                        for(const p of points){
+                            let d = Math.sqrt((x-p.x)**2+(y-p.y)**2);
+                            if(d<3) add = false;
+                        }
+                        if(add)points.push(new Point(x, y));
+                        vor.point_list = points;
+                    
+                    
+                        let t0 = performance.now();
+                    
+                        vor.update();
+                    
+                        let t1 = performance.now();
+                    
+                        gr.draw(points, vor.voronoi_vertex, vor.edges, vor.maxCircle);
+                    
+                        $("#timer").text((t1 - t0).toFixed(2) + " ms");
+
+                        // vor1.add_point(new PointD(x, y, 2));
+                        // vor1.partial_update(y);
+                        // vor1.add_point(new PointD(w1,h1*0.3,2));
+                        // vor1.partial_update(h1*0.8);
+
+                        // event handlers
+                        voronoi.addSite(x, y);
+                    }
+
+                    // console.log(userPoints)
+                }
+
+                fr.readAsText(this.files[0]);
+            })
 });
 
 // $(document).ready(function () {
